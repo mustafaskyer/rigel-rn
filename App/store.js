360e26fa37dad7fe22065e6b7da3152b6b2eb39c
skyermustafa <mustafaskir@gmail.com>
2018-12-15 13:18:07 +0300

    Added Immutable Trandforms to persist config,

--- App/store.js
+++ App/store.js
@@ -1,21 +1,25 @@
-import { createStore, applyMiddleware } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import createSagaMiddleware from 'redux-saga'
-import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
-
-import saga from './redux/sgags/index'
-import rootReducer from './redux/reducers/index';
+import { createStore, applyMiddleware } from "redux";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import createSagaMiddleware from "redux-saga";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+import saga from "./redux/sgags/index";
+import rootReducer from "./redux/reducers/index";
+import immutableTransform from 'redux-persist-transform-immutable'
 
 const sagaMiddleware = createSagaMiddleware();
 const persistConfig = {
- key: 'root',
- storage: storage,
- stateReconciler: autoMergeLevel2,
+  key: "root",
+  storage: storage,
+  stateReconciler: autoMergeLevel2,
+  transforms: [immutableTransform()],
 };
 
 const persistReducerr = persistReducer(persistConfig, rootReducer);
-export const store = createStore(persistReducerr,{},applyMiddleware(sagaMiddleware));
+export const store = createStore(
+  persistReducerr,
+  {},
+  applyMiddleware(sagaMiddleware)
+);
 export const persistor = persistStore(store);
-
-sagaMiddleware.run(saga)
+sagaMiddleware.run(saga);
