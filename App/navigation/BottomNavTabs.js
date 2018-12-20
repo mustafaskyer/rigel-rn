4c24b1b773fe284c7c937bd13308dc01a5d10a5f
skyermustafa <mustafaskir@gmail.com>
2018-12-21 01:12:43 +0300

    recompose bottomNavTabs, connected with redux, added Badges, and defaultTabIndex

--- App/navigation/BottomNavTabs.js
+++ App/navigation/BottomNavTabs.js
@@ -1,64 +1,118 @@
 import React from "react";
-import { View } from "react-native";
-import Icon from "react-native-vector-icons/MaterialIcons";
-
+import { View, Text } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+import { withProps, withStateHandlers, compose, lifecycle } from 'recompose';
+import _ from 'lodash';
 import BottomNavigation, {
-  ShiftingTab
+  ShiftingTab,
+  Badge
 } from "react-native-material-bottom-navigation";
+import { connect } from 'react-redux';
 
-export default class App extends React.Component {
-  state = { activeTab: "one" };
-  tabs = [
-    {
-      key: "Home",
-      icon: "list",
-      label: "Home",
-      barColor: "#55bbff",
-      pressColor: "rgba(255, 255, 255, 0.16)"
-    },
-    {
-      key: "Login",
-      icon: "music-note",
-      label: "Music",
-      barColor: "#388E3C",
-      pressColor: "rgba(255, 255, 255, 0.16)"
-    },
-    {
-      key: "LoginForm",
-      icon: "movie",
-      label: "Movies & TV",
-      barColor: "#B71C1C",
-      pressColor: "rgba(255, 255, 255, 0.16)"
-    }
-  ];
+const tabs = [
+  {
+    key: "Search",
+    icon: "ios-search",
+    label: "Search",
+    barColor: "#388E3C",
+    pressColor: "rgba(255, 255, 255, 0.16)",
+  },
+  {
+    key: "Notifications",
+    icon: "ios-notifications",
+    label: "Notifications",
+    barColor: "#B71C1C",
+    pressColor: "rgba(255, 255, 255, 0.16)",
+    badge: 19
+  },
+  {
+    key: "Home",
+    icon: "ios-home",
+    label: "Home",
+    barColor: "#55bbff",
+    pressColor: "rgba(255, 255, 255, 0.16)",
+  },
+  {
+    key: "Likes",
+    icon: "ios-heart",
+    label: "Likes",
+    barColor: "#B71C1C",
+    pressColor: "rgba(255, 255, 255, 0.16)",
+    badge: 77
+  },
+  {
+    key: "profile",
+    icon: "ios-contact",
+    label: "Profile",
+    barColor: "#B71C1C",
+    pressColor: "rgba(255, 255, 255, 0.16)",
+  },
+];
 
-  renderIcon = icon => ({ isActive }) => (
+
+const  BottomTabs = (props) => {
+  
+  const renderIcon = icon => ({ isActive }) => (
     <Icon size={24} color="white" name={icon} />
   );
 
-  renderTab = ({ tab, isActive }) => (
-    <View style={{ flex: 1 }}>
-      <ShiftingTab
-        isActive={isActive}
-        key={tab.key}
-        label={tab.label}
-        renderIcon={this.renderIcon(tab.icon)}
-      />
-    </View>
-  );
-
-  render() {
+  renderTab = ({ tab, isActive }) => {
     return (
-      //   <View style={{ flex: 1 }}>
-      //     <View style={{ flex: 1 }}>
-      //       {/* {this._renderScreens()} */}
-      //     </View>
-      <BottomNavigation
-        onTabPress={newTab => this.props.navigation.navigate(newTab.key)}
-        renderTab={this.renderTab}
-        tabs={this.tabs}
-      />
-      //   </View>
-    );
+      <View style={{ flex: 1 }}>
+        <ShiftingTab
+          isActive={isActive}
+          key={tab.key}
+          label={tab.label}
+          renderIcon={renderIcon(tab.icon)}
+          renderBadge={() => {
+            return tab.badge && (
+              <Badge style={{ backgroundColor:'#FFF' }}><Text>{tab.badge}</Text></Badge>
+            )
+          }}
+          showBadge={true}
+        />
+      </View>
+    )
+  };
+
+  return(
+    <BottomNavigation
+        onTabPress={newTab => {
+          props.setActiveTab(newTab.key)
+          props.navigation.navigate(newTab.key)
+        }}
+        renderTab={renderTab}
+        tabs={tabs}
+        defaultTabIndex={2}
+    />
+  )
+}
+
+const comp = compose(
+  withProps(props => ({ ...props })),
+  withStateHandlers(props => ({ activeTab: 'Home' }), {
+    setActiveTab: props => ev => {
+      let badges = [ { label:'Home', badge: 17 } , {label:'Likes', badge: '77' } ]
+      let isActive = _.find(badges, { label: ev })
+      let badge = isActive && isActive.badge
+      
+      return { ...props, activeTab: ev, badge }
+    }
+  }),
+  lifecycle({
+    componentDidUpdate(prevProps, prevState) {
+      console.log('@props', prevProps)
+      console.log('@state', prevState)
+    },
+    componentWillMount() {
+      // this.setState()
+    }
+  })
+)(BottomTabs)
+
+const mapStateToProps = state => {
+  return {
+    data: state.dd
   }
 }
+export default connect(mapStateToProps)(comp)
