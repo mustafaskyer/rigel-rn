71f26585370d8fac69821122daf38cca4eed9510
skyermustafa <mustafaskir@gmail.com>
2018-12-21 15:01:05 +0300

    Created Badges Reducer

--- App/redux/reducers/index.js
+++ App/redux/reducers/index.js
@@ -4,6 +4,7 @@ import Immutable from "immutable";
 // func that take object and return .toJs
 
 const rootReducer = combineReducers({
-  dd: () => Immutable.Map({ arr: [1, 2, 3, 4, 5, 6] }).toJS()
+  dd: () => Immutable.Map({ arr: [1, 2, 3, 4, 5, 6] }).toJS(),
+  badges: () => Immutable.Map({ badges: [{ label:'Home', badge: 3 } , {label:'Likes', badge: '11' }] }).toJS()
 });
 export default rootReducer;
