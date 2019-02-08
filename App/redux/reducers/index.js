4dfa24734354c6be6af6aa73ba02b38553a4fe31
skyermustafa <mustafaskir@gmail.com>
2019-02-09 02:29:33 +0300

    updated combineReducers, removed immutable, now immer works

--- App/redux/reducers/index.js
+++ App/redux/reducers/index.js
@@ -1,10 +1,7 @@
 import { combineReducers } from "redux";
-import Immutable from "immutable";
-
-// func that take object and return .toJs
+import badges from './Badges';
 
 const rootReducer = combineReducers({
-  dd: () => Immutable.Map({ arr: [1, 2, 3, 4, 5, 6] }).toJS(),
-  badges: () => Immutable.Map({ badges: [{ label:'Notifications', badge: 3 } , {label:'Likes', badge: '11' }] }).toJS()
+  badges
 });
 export default rootReducer;
