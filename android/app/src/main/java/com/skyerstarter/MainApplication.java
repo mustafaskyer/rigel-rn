7b50e67e3c9cd56bc8316bc7f2ddd45a3d6f375b
skyermustafa <mustafaskir@gmail.com>
2019-02-10 19:07:34 +0300

    lottie linking updates

--- android/app/src/main/java/com/skyerstarter/MainApplication.java
+++ android/app/src/main/java/com/skyerstarter/MainApplication.java
@@ -3,6 +3,7 @@ package com.skyerstarter;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.lottie.LottiePackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LottiePackage(),
             new RNI18nPackage(),
             new VectorIconsPackage(),
             new ReanimatedPackage(),
