c4c77a89793f4beda68fee979c155944ee94b2c4
Mustafa <mustafaskir@gmail.com>
2019-07-20 01:22:39 +0300

    fixed android, works as expected ✅

--- android/app/src/main/java/com/skyerstarter/MainApplication.java
+++ android/app/src/main/java/com/skyerstarter/MainApplication.java
@@ -3,10 +3,9 @@ package com.skyerstarter;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.swmansion.reanimated.ReanimatedPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.airbnb.android.react.lottie.LottiePackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
+import com.swmansion.reanimated.ReanimatedPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -28,10 +27,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ReanimatedPackage(),
             new RNGestureHandlerPackage(),
-            new LottiePackage(),
             new RNI18nPackage(),
+            new ReanimatedPackage(),
             new VectorIconsPackage()
       );
     }
