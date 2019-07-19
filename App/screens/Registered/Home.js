c07b731e0eb2d78ae1d6804a78b764bf3bf1fdbb
Mustafa <mustafaskir@gmail.com>
2019-07-20 02:50:09 +0300

    added basic screens for registered  ✅

---
+++ App/screens/Registered/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> Home </Text>
+      </View>
+    );
+  }
+}
---
+++ App/screens/Registered/index.js
@@ -0,0 +1,4 @@
+import Home from './Home';
+import Settings from './Settings';
+
+export default { Home, Settings }
