c5eea7592f6d46c7fed2ad4c88c46d894e221c61
Mustafa <mustafaskir@gmail.com>
2019-07-21 22:54:53 +0300

    added linear gradient ✨

--- android/app/src/main/java/com/skyerstarter/MainApplication.java
+++ android/app/src/main/java/com/skyerstarter/MainApplication.java
@@ -3,6 +3,7 @@ package com.skyerstarter;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new RNGestureHandlerPackage(),
             new RNI18nPackage(),
             new ReanimatedPackage(),
