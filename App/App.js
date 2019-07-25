b7acfe2154421a145340e45db408c653bd1d8501
Mustafa <mustafaskir@gmail.com>
2019-07-25 22:34:15 +0300

    removed shoute theme and styles ☑️

--- App/App.js
+++ App/App.js
@@ -1,19 +1,8 @@
 import React, { Component } from 'react';
-import { Dimensions } from 'react-native';
-
 import EStyleSheet from 'react-native-extended-stylesheet';
 //@colors&fonts
 import colors from './assets/styles/colors';
 import fonts from './assets/styles/fonts';
-
-import { StyleProvider } from '@shoutem/theme';
-import {
-  Card,
-  Image,
-  View,
-  Subtitle,
-  Caption,
-} from '@shoutem/ui';
 
 /**
  * @redux
@@ -26,72 +15,7 @@ import { persistor, store } from './store';
 //i18n
 import i18n from 'react-native-i18n';
 import Navigator from './navigation/Navigator';
-
-/**
- * Theme Adds
- */
-const window = Dimensions.get('window');
-const Colors = {
-  BACKGROUND: '#ffffff',
-  SHADOW: '#000000',
-};
-
-const MEDIUM_GUTTER = 15;
-
-const theme = {
-  'shoutem.ui.View': {
-    '.h-center': {
-      alignItems: 'center',
-    },
-
-    '.v-center': {
-      justifyContent: 'center',
-    },
-
-    '.flexible': {
-      flex: 1,
-    },
-
-    flexDirection: 'column',
-  },
-
-  'shoutem.ui.Card': {
-    'shoutem.ui.View.content': {
-      'shoutem.ui.Subtitle': {
-        marginBottom: MEDIUM_GUTTER,
-      },
-
-      flex: 1,
-      alignSelf: 'stretch',
-      padding: 10,
-    },
-
-    width: (180 / 375) * window.width,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    backgroundColor: Colors.BACKGROUND,
-    borderRadius: 2,
-    shadowColor: Colors.SHADOW,
-    shadowOpacity: 0.1,
-    shadowOffset: { width: 1, height: 1 },
-  },
-
-  'shoutem.ui.Image': {
-    '.medium-wide': {
-      width: (180 / 375) * window.width,
-      height: 85,
-    },
-
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: Colors.BACKGROUND,
-  },
-};
-
-
-
+/** Disabled console warnings */
 console.disableYellowBox = true;
 export default class App extends Component {
   constructor(){
@@ -106,9 +30,7 @@ export default class App extends Component {
     return (
       <Provider store={store}>
           <PersistGate loading={false} persistor={persistor}>
-          <StyleProvider style={theme}>
             <Navigator />
-          </StyleProvider>
           </PersistGate>
       </Provider>
     );
