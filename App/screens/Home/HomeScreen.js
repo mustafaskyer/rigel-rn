373fae578dae6afdf9c756128e789964821ad61b
Mustafa <mustafaskir@gmail.com>
2019-07-27 23:08:16 +0300

    added succeffully  ✅

---
+++ App/screens/Home/HomeScreen.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+import styles from './HomeStyles';
+import { connect } from 'react-redux';
+
+function HomeScreen(props){
+    const [state, setState] = useState(null)
+    useEffect(() => {
+        /**  */
+    })
+    return(
+        <View style={styles.container}>
+            <Text>{'HomeScreen'}</Text>
+        </View>
+    )
+}
+
+
+/**
+ * @param {any} state 
+ * @returns 
+ */
+const mapProps = state => {
+    return {
+
+    }
+}
+export default connect(mapProps)(HomeScreen)
---
+++ App/screens/Settings/SettingsScreen.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+import styles from './SettingsStyles';
+import { connect } from 'react-redux';
+
+function SettingsScreen(props){
+    const [state, setState] = useState(null)
+    useEffect(() => {
+        /**  */
+    })
+    return(
+        <View style={styles.container}>
+            <Text>{'SettingsScreen'}</Text>
+        </View>
+    )
+}
+
+
+/**
+ * @param {any} state 
+ * @returns 
+ */
+const mapProps = state => {
+    return {
+
+    }
+}
+export default connect(mapProps)(SettingsScreen)
