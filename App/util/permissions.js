1f319e559be384cc64e85c7db61c904cd46f3463
Mustafa <mustafaskir@gmail.com>
2019-07-29 00:36:03 +0300

    updated permission util

--- App/util/permissions.js
+++ App/util/permissions.js
@@ -23,21 +23,30 @@ import Permissions from "react-native-permissions";
     Storage	        storage	                        ❌️	    ✔
  */
 const permissionsModule = (function() {
-  async function request(type) {
-    const res = await Permissions.request(type);
-    return res
-  }
-  async function check(type) {
-    const res = await Permissions.request(type);
+  async function request(name, title, message, type) {
+    const res = await Permissions.request(name, {
+      rationale: { title, message },
+      type: type,
+    //   type // incase location accept [always or whenInUse] 
+    //   type // incase notification accept [alert, badge, sound] 
+    });
     return res;
   }
-  async function checkMultiple(types) {
-    const res = await Permissions.checkMultiple(types);
+  async function check(name, type) {
+    const res = await Permissions.request(name, {
+        type,
+        //   type // incase location accept [always or whenInUse] 
+        //   type // incase notification accept [alert, badge, sound] 
+    });
+    return res;
+  }
+  async function checkMultiple(names) {
+    const res = await Permissions.checkMultiple(names);
     return res;
   }
   async function openSettings() {
     const res = await Permissions.openSettings;
-    return res
+    return res;
   }
   async function getTypes() {
     const res = await Permissions.getTypes();
