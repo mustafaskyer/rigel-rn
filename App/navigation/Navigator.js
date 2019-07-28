00b14bffd858f8f2b75d9875e2a36a4ec4cfd574
Mustafa <mustafaskir@gmail.com>
2019-07-28 23:36:31 +0300

    updated with iphone-x case  ✅

--- App/navigation/Navigator.js
+++ App/navigation/Navigator.js
@@ -1,28 +1,38 @@
 import React, { useEffect } from "react";
+import { View, StyleSheet } from "react-native";
 import { connect } from "react-redux";
 import Navigator from "./AppNavigation";
-import {
-  useAppState,
-  useGeolocation,
-  useNetInfo
-} from "react-native-hooks";
+import { useAppState, useNetInfo } from "react-native-hooks";
+import { ifIphoneX } from "react-native-iphone-x-helper";
 import { appStateAction } from "redux-actions";
 
 const Nav = props => {
   const currentAppState = useAppState();
-  const [position] = useGeolocation();
   const netInfo = useNetInfo();
 
   useEffect(() => {
     props.appStateAction({
       appState: currentAppState,
-      netInfo,
-      position,
+      netInfo
     });
   });
 
-  return <Navigator {...props} />;
+  return (
+    <View style={styles.view}>
+      <Navigator {...props} />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+    view: {
+      flex: 1,
+      ...ifIphoneX({
+        paddingTop: 44,
+        paddingBottom: 34
+      })
+    }
+  });
 
 export default connect(
   null,
