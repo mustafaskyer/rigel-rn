f3bbc90e919166e1fd5aaba98fac73d70d5bcbab
Mustafa <mustafaskir@gmail.com>
2019-07-29 00:27:06 +0300

    updated navigator with moreAppState  ✅

--- App/navigation/Navigator.js
+++ App/navigation/Navigator.js
@@ -2,18 +2,23 @@ import React, { useEffect } from "react";
 import { View, StyleSheet } from "react-native";
 import { connect } from "react-redux";
 import Navigator from "./AppNavigation";
-import { useAppState, useNetInfo } from "react-native-hooks";
+import { useAppState, useNetInfo, useGeolocation } from "react-native-hooks";
 import { ifIphoneX } from "react-native-iphone-x-helper";
 import { appStateAction } from "redux-actions";
+// import permissions from 'util/permissions';
 
+
+// permissions.check('storage').then(r => console.log('@r', r))
 const Nav = props => {
-  const currentAppState = useAppState();
-  const netInfo = useNetInfo();
-
-  useEffect(() => {
+    const currentAppState = useAppState();
+    const netInfo = useNetInfo();
+    const [position] = useGeolocation()
+    
+    useEffect(() => {
     props.appStateAction({
       appState: currentAppState,
-      netInfo
+      netInfo,
+      position
     });
   });
 
