f4f9e21cff74dcd448a48b15796a86850e0c14e8
Mustafa <mustafaskir@gmail.com>
2019-07-30 00:19:50 +0300

    upgraded to rn 0.60.3  ✅

--- android/app/src/main/java/com/skyerstarter/MainApplication.java
+++ android/app/src/main/java/com/skyerstarter/MainApplication.java
@@ -1,7 +1,11 @@
 package com.skyerstarter;
 
 import android.app.Application;
+import android.util.Log;
 
+import com.facebook.react.PackageList;
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -11,10 +15,9 @@ import com.swmansion.reanimated.ReanimatedPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
+
 import com.facebook.soloader.SoLoader;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -27,15 +30,28 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new AsyncStoragePackage(),
-            new LinearGradientPackage(),
-            new RNGestureHandlerPackage(),
-            new RNI18nPackage(),
-            new ReanimatedPackage(),
-            new VectorIconsPackage()
-      );
+
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new AsyncStoragePackage());
+      // packages.add(new LinearGradientPackage());
+      // packages.add(new RNGestureHandlerPackage());
+      // packages.add(new RNI18nPackage());
+      // packages.add(new ReanimatedPackage());
+      // packages.add(new VectorIconsPackage());
+      return packages;
+
+      // return Arrays.<ReactPackage>asList(
+      //     new MainReactPackage(),
+      //       new AsyncStoragePackage(),
+      //       new LinearGradientPackage(),
+      //       new RNGestureHandlerPackage(),
+      //       new RNI18nPackage(),
+      //       new ReanimatedPackage(),
+      //       new VectorIconsPackage()
+      // );
+
     }
 
     @Override
