7e2e954f9487fcdb7b448715220c78885b670c88
Mustafa <mustafaskir@gmail.com>
2019-07-29 18:45:16 +0300

    platform detected  ✅

--- App/navigation/Navigator.js
+++ App/navigation/Navigator.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Platform } from "react-native";
 import { connect } from "react-redux";
 import Navigator from "./AppNavigation";
 import { useAppState, useNetInfo, useGeolocation } from "react-native-hooks";
@@ -7,18 +7,18 @@ import { ifIphoneX } from "react-native-iphone-x-helper";
 import { appStateAction } from "redux-actions";
 // import permissions from 'util/permissions';
 
-
 // permissions.check('storage').then(r => console.log('@r', r))
 const Nav = props => {
-    const currentAppState = useAppState();
-    const netInfo = useNetInfo();
-    const [position] = useGeolocation()
-    
-    useEffect(() => {
+  const currentAppState = useAppState();
+  const netInfo = useNetInfo();
+  const [position] = useGeolocation();
+
+  useEffect(() => {
     props.appStateAction({
       appState: currentAppState,
       netInfo,
-      position
+      position,
+      plaform: Platform.OS
     });
   });
 
@@ -30,14 +30,14 @@ const Nav = props => {
 };
 
 const styles = StyleSheet.create({
-    view: {
-      flex: 1,
-      ...ifIphoneX({
-        paddingTop: 44,
-        paddingBottom: 34
-      })
-    }
-  });
+  view: {
+    flex: 1,
+    ...ifIphoneX({
+      paddingTop: 44,
+      paddingBottom: 34
+    })
+  }
+});
 
 export default connect(
   null,
