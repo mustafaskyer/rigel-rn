5dba11eca9e8345d648388b755cb62a5ce43ab7f
Mustafa <mustafaskir@gmail.com>
2019-07-31 18:43:06 +0300

    removed mapModule

--- App/screens/Home/HomeScreen.js
+++ App/screens/Home/HomeScreen.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import styles from "./HomeStyles";
 import { connect } from "react-redux";
+// import MapBoxGl from 'modules/mapbox';
 function HomeScreen(props) {
   const [state, setState] = useState(null);
   useEffect(() => {
@@ -9,6 +10,7 @@ function HomeScreen(props) {
   });
   return (
     <View style={styles.container}>
+      {/* <MapBoxGl coords={[46.71473915, 24.75040361]} /> */}
       <Text>{'HomeScreen'}</Text>
     </View>
   );
@@ -21,4 +23,9 @@ function HomeScreen(props) {
 const mapProps = state => {
   return {};
 };
+
+/**
+ * Example
+ * <RenderIcon imgSource={cImg} title={'Logo'} coordinate={props.coords}/>
+ */
 export default connect(mapProps)(HomeScreen);
