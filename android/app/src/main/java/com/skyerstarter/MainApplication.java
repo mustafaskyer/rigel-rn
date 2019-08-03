8767f72295f13ec6f9bf3383269d9a480682b050
Mustafa <mustafaskir@gmail.com>
2019-08-03 22:24:14 +0300

    removed netInfo pckg, fixed android crash  ✅

--- android/app/src/main/java/com/skyerstarter/MainApplication.java
+++ android/app/src/main/java/com/skyerstarter/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.reactnativecommunity.cameraroll.CameraRollPackage;
-import com.reactnativecommunity.netinfo.NetInfoPackage;
 import com.reactnativecommunity.geolocation.GeolocationPackage;
 import com.mapbox.rctmgl.RCTMGLPackage;
 import com.zmxv.RNSound.RNSoundPackage;
@@ -35,7 +34,6 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new CameraRollPackage(),
-            new NetInfoPackage(),
             new GeolocationPackage(),
             new RCTMGLPackage(),
             new RNSoundPackage(),
