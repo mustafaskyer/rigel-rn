d4b432972d0c1627d8eaf03f07269f8ae9760fdc
Mustafa <mskyer@protonmail.com>
2019-09-03 10:37:47 +0300

    updated notifications components

--- App/components/NotificationsComponent.js
+++ App/components/NotificationsComponent.js
@@ -1,83 +1,125 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Text, View, StyleSheet, TouchableWithoutFeedback } from "react-native";
 import Animated from "react-native-reanimated";
 import MaterialIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import runTiming from "util/animations/runTiming";
 import { connect } from "react-redux";
+import { clearNotifications } from "redux-actions";
 
-const { interpolate, Clock, Extrapolate, concat } = Animated;
+const { interpolate, Clock, Extrapolate, concat, Value } = Animated;
 
-function NotificationsComponent(props) {
-  const [trans, setTrans] = useState(-300);
-  const [rotate, setRotate] = useState(-300);
-  const {message,type} = props.notifications.payload && props.notifications.payload || {}
-  const scaleY = interpolate(trans, {
-    inputRange: [-300, -50, 0],
-    outputRange: [0, 0, 1],
-    extrapolate: Extrapolate.CLAMP
-  });
-  const rotateInterpolate = interpolate(rotate, {
-    inputRange: [0, 0.3, 0.7, 0.9, 1],
-    outputRange: [-45, 45, -45, 0, 20],
-    extrapolate: Extrapolate.CLAMP
-  });
-  const textFadeInterpolate = interpolate(rotate, {
-    inputRange: [0, 1],
-    outputRange: [0, 1],
-    extrapolate: Extrapolate.CLAMP
-  });
-  useEffect(() => {
-    if (props.notifications.payload) {
-      setTrans(runTiming(new Clock(rotate), -300, 0, 750));
-      setRotate(runTiming(new Clock(), 0, 1, 1750));
-      setTimeout(() => {
-        setTrans(runTiming(new Clock(), 0, -300, 0));
-      }, 4000);
+class NComp extends React.Component {
+  constructor(props) {
+    super(props);
+    this._watchClear = null;
+    this.state = {
+      trans: new Value(-300),
+      rotate: new Value(0)
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.notifications.payload) {
+      if (this.state.rotate === 0) {
+        this.setState({
+          trans: runTiming(new Clock(), 0, -300, 750)
+        });
+      }
+      this.setState(
+        {
+          trans: runTiming(new Clock(this.state.rotate), -300, 0, 750),
+          rotate: runTiming(new Clock(), 0, 1, 1750)
+        },
+        () => {
+          this._watchClear = setTimeout(() => {
+            this.setState({ trans: new Value(-300), rotate: new Value(0) });
+          }, 5000);
+        }
+      );
     }
-  }, [props.notifications.payload]);
-  return (
-    <Animated.View
-      style={[styles.container, { top: trans, transform: [{ scaleY }], backgroundColor: type === 'info' ? 'green' : 'red' }]}
-    >
-      <TouchableWithoutFeedback
-        onPress={() => setTrans(runTiming(new Clock(), 0, -300, 750))}
+  }
+
+  componentWillUpdate(props, state) {
+    if (props.notifications.payload) {
+      clearInterval(this._watchClear);
+    }
+  }
+
+  componentWillUnmount() {
+    this.props.clearNotifications();
+  }
+  render() {
+    this.scaleY = interpolate(this.state.trans, {
+      inputRange: [-300, -50, 0],
+      outputRange: [0, 0, 1],
+      extrapolate: Extrapolate.CLAMP
+    });
+    this.rotateInterpolate = interpolate(this.state.rotate, {
+      inputRange: [0, 0.3, 0.7, 0.9, 1],
+      outputRange: [-45, 45, -45, 0, 20],
+      extrapolate: Extrapolate.CLAMP
+    });
+    this.textFadeInterpolate = interpolate(this.state.rotate, {
+      inputRange: [0, 1],
+      outputRange: [0, 1],
+      extrapolate: Extrapolate.CLAMP
+    });
+    return (
+      <Animated.View
+        style={[
+          styles.container,
+          { top: this.state.trans, transform: [{ scaleY: this.scaleY }] },
+          this.props.notifications.payload &&
+            this.props.notifications.payload.type === "error" && {
+              backgroundColor: "red"
+            }
+        ]}
       >
-        <View style={styles.bar}>
-          <Animated.View
-            style={{
-              transform: [{ rotate: concat(rotateInterpolate, "deg") }],
-              opacity: textFadeInterpolate
-            }}
-          >
-            <MaterialIcons name="bell-ring" size={19} color="#FFF" />
-          </Animated.View>
-          <View style={styles.textView}>
-            {props.notifications.payload && (
-              <Animated.Text
-                style={[styles.barText, { opacity: textFadeInterpolate }]}
-              >
-                {message}
-              </Animated.Text>
-            )}
+        <TouchableWithoutFeedback
+          onPress={() => {
+            this.setState({ trans: runTiming(new Clock(), 0, -300, 750) });
+          }}
+        >
+          <View style={styles.bar}>
+            <Animated.View
+              style={{
+                transform: [{ rotate: concat(this.rotateInterpolate, "deg") }],
+                opacity: this.textFadeInterpolate
+              }}
+            >
+              <MaterialIcons name="bell-ring" size={19} color="#FFF" />
+            </Animated.View>
+            <View style={styles.textView}>
+              {this.props.notifications.payload && (
+                <Animated.Text
+                  style={[
+                    styles.barText,
+                    { opacity: this.textFadeInterpolate }
+                  ]}
+                >
+                  {this.props.notifications.payload.message}
+                </Animated.Text>
+              )}
+            </View>
           </View>
-        </View>
-      </TouchableWithoutFeedback>
-    </Animated.View>
-  );
+        </TouchableWithoutFeedback>
+      </Animated.View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     width: "100%",
     height: 40,
-    backgroundColor: "green",
+    backgroundColor: "green"
   },
   bar: {
     flexDirection: "row",
     alignItems: "center",
     paddingStart: 11,
     flex: 1,
-    width: "100%",
+    width: "100%"
   },
   barText: {
     fontSize: 15,
@@ -102,5 +144,5 @@ const mapProps = state => {
 
 export default connect(
   mapProps,
-  null
-)(NotificationsComponent);
+  { clearNotifications }
+)(NComp);
