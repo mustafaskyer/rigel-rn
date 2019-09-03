d6f9c5e0cc61c1099ca8449c990b2eefd6104c01
Mustafa <mskyer@protonmail.com>
2019-09-03 15:30:11 +0300

    addeed onesignal, works as expected ✅

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Mustafa Skyer on 9/3/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
