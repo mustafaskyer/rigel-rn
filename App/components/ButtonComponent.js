9be5e86d11be030dfe0bc404d1df068548a542b2
Mustafa Elsayed <mskyer@protonmail.com>
2020-01-25 22:43:20 +0300

    eslint --fix

--- App/components/ButtonComponent.js
+++ App/components/ButtonComponent.js
@@ -1,37 +1,46 @@
-import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity, ActivityIndicator } from "react-native";
-import { getBottomSpace } from 'react-native-iphone-x-helper';
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ActivityIndicator,
+} from 'react-native';
+import {getBottomSpace} from 'react-native-iphone-x-helper';
 
 function ButtonComponent(props) {
   return (
     <TouchableOpacity {...props} style={[styles.container, props.style]}>
-      {
-        props.loading 
-        ? <ActivityIndicator color={props.spinnerColor} size={props.spinnerSize} />
-        : <Text style={styles.btnText}>{props.title}</Text>
-      }
+      {props.loading ? (
+        <ActivityIndicator
+          color={props.spinnerColor}
+          size={props.spinnerSize}
+        />
+      ) : (
+        <Text style={styles.btnText}>{props.title}</Text>
+      )}
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#BF4C58",
+    backgroundColor: '#BF4C58',
     minWidth: '100%',
-    height: 55 + getBottomSpace()/3 ,
-    paddingBottom:  getBottomSpace()/3,
+    height: 55 + getBottomSpace() / 3,
+    paddingBottom: getBottomSpace() / 3,
     alignSelf: 'center',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   btnText: {
-    color: "#FFFFFF",
-    fontFamily: "ExpoArabic-Medium",
+    color: '#FFFFFF',
+    fontFamily: 'ExpoArabic-Medium',
     fontSize: 14,
-    fontWeight: "500",
+    fontWeight: '500',
     lineHeight: 17,
-    textAlign: "center"
-  }
+    textAlign: 'center',
+  },
 });
 
 export default ButtonComponent;
