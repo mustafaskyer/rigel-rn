2dacfcfcc2090742ae9bbc21a3303cbf6791c79f
Mustafa Elsayed <mskyer@protonmail.com>
2020-01-25 22:43:00 +0300

    eslint --fix

--- app.config.js
+++ app.config.js
@@ -3,10 +3,10 @@ const hasTabs = false;
 /** control navigation should have drawer or not */
 const hasDrawer = true;
 /** set default app lang */
-const defaultLang = "ar";
+const defaultLang = 'ar';
 /** set default app theme color */
-const themeColor = "#FFF";
+const themeColor = '#FFF';
 /** set second app theme color */
-const secondColor = "#ddd";
+const secondColor = '#ddd';
 
-module.exports = { hasTabs, hasDrawer, defaultLang, themeColor, secondColor };
+module.exports = {hasTabs, hasDrawer, defaultLang, themeColor, secondColor};
--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,28 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  "plugins": [
-    ["module-resolver", {
-      "root": ["./App"],
-      "alias": {
-        "root": "./App",
-        "screens": "./App/screens",
-        "components": "./App/components",
-        "styles": "./App/assets/styles",
-        "imgs": "./App/assets/imgs",
-        "langs": "./App/langs/index",
-        "redux-actions": "./App/redux/actions",
-        "redux-reducer": "./App/redux/reducers",
-        "redux-types": "./App/redux/REDUX_TYPES",
-        "configs": "./App/configs",
-        "api": "./App/redux/apis/",
-        "util":"./App/util",
-        "config": "./app.config.js"
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./App'],
+        alias: {
+          root: './App',
+          screens: './App/screens',
+          components: './App/components',
+          styles: './App/assets/styles',
+          imgs: './App/assets/imgs',
+          svg: './App/assets/imgs',
+          langs: './App/langs/svg',
+          'redux-actions': './App/redux/actions',
+          'redux-reducer': './App/redux/reducers',
+          'redux-types': './App/redux/types/index',
+          configs: './App/configs',
+          api: './App/redux/apis/',
+          util: './App/util',
+          config: './app.config.js',
+        },
+        extensions: ['.js'],
       },
-      "extensions": [".js"]
-    }]
-  ]
+    ],
+  ],
 };
