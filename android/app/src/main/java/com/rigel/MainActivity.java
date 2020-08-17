25ebd58b5784b03bdac22e4603bfc219413378f7
Mustafa Elsayed <mskyer@protonmail.com>
2020-08-17 11:27:38 +0300

    upgraded to 0.63.0

--- android/app/src/main/java/com/rigel/MainActivity.java
+++ android/app/src/main/java/com/rigel/MainActivity.java
@@ -1,4 +1,4 @@
-package com.skyerstarterrn;
+package com.rigel;
 
 import com.facebook.react.ReactActivity;
 import android.os.Bundle;
@@ -11,7 +11,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "SkyerStarterRN";
+    return "Rigel";
   }
   // @Override
   // protected void onCreate(Bundle savedInstanceState) {
